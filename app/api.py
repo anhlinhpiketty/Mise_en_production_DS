ecce912790a235976e5b3b29649b504dae1967bd
Colas-Lepoutre <colas.lepoutre71@gmail.com>
2026-04-17 12:30:51 +0000

    ajout description api

--- app/api.py
+++ app/api.py
@@ -28,7 +28,7 @@ def show_welcome_page():
     return {
         "Message": "API d'extraction et de classification de compétences issues d'offres d'emploi",
         "Model_name": "NER_LLM_JOCAS",
-        "Model_version": "1.0",
+        "Model_version": "1.0-llm",
     }
 
 @app.get("/analyze", tags=["Analyze"])
